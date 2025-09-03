16effd3d3cc5a6d81c8402b7d2e4be44505c7250
david.rodriguez <nedaroga@gmail.com>
2025-09-02 20:58:34 -0400

    Cloud configuration

--- src/save_profile.py
+++ src/save_profile.py
@@ -26,6 +26,7 @@ def save_profile(email, target_language, base_language, exam_scope, current_leve
 
 def get_user_profile(email):
     creds = st.secrets["gcp_service_account"]
+    st.write(st.secrets["gcp_service_account"]["private_key"])
     client = gspread.service_account_from_dict(creds)
     db = client.open_by_key("1pNgSoEpnTEG17j06niCj7_fCUN69DZ-Dl3t4DPk9l5w").sheet1
     records = db.get_all_records()  # lee toda la hoja
