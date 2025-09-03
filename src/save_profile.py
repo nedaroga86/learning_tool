80fc76e667d7e9b0ddfa18d83d910680fd301b88
david.rodriguez <nedaroga@gmail.com>
2025-09-02 21:25:56 -0400

    Cloud configuration

--- src/save_profile.py
+++ src/save_profile.py
@@ -2,6 +2,7 @@ import os
 import time
 import gspread
 import streamlit as st
+from google.oauth2 import service_account
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -19,15 +20,14 @@ scope = ["https://www.googleapis.com/auth/spreadsheets",
 # Guarda perfil
 def save_profile(email, target_language, base_language, exam_scope, current_level, goal_level, prep_time):
     creds = st.secrets["gcp_service_account"]
-    client = gspread.service_account_from_dict(creds)
+    client = service_account.Credentials.from_service_account_info(dict(creds))
     db = client.open_by_key("1pNgSoEpnTEG17j06niCj7_fCUN69DZ-Dl3t4DPk9l5w").sheet1
     db.append_row([email, target_language, base_language, exam_scope,
                       current_level, goal_level, prep_time])
 
 def get_user_profile(email):
     creds = st.secrets["gcp_service_account"]
-    st.write(st.secrets["gcp_service_account"]["private_key"])
-    client = gspread.service_account_from_dict(creds)
+    client = service_account.Credentials.from_service_account_info(dict(creds))
     db = client.open_by_key("1pNgSoEpnTEG17j06niCj7_fCUN69DZ-Dl3t4DPk9l5w").sheet1
     records = db.get_all_records()  # lee toda la hoja
     for row in records:
